c44c22b6a656dca0c5c51607ecf40e9430276798
mmarti78 <mohamed_martini@student.uml.edu>
2020-11-27 03:33:54 -0500

    refactoring

--- rpi_recovery.py
+++ rpi_recovery.py
@@ -1,6 +1,7 @@
 import os
 
 requirements_path = r"/home/pi/Desktop/Cabinet/requirements.txt"
+requirements_path = r"requirements.txt"
 
 with open(requirements_path) as reqs:
     for line in reqs.readlines():
