9f871e319da525972be415c733198d6088e896d6
mmarti78 <mohamed_martini@student.uml.edu>
2020-11-27 03:24:26 -0500

    rpi_recovery.py Script to install App requirements

--- rpi_recovery.py
+++ rpi_recovery.py
@@ -1,9 +1,8 @@
 import os
 
 requirements_path = r"/home/pi/Desktop/Cabinet/requirements.txt"
-requirements_path = r"requirements.txt"
 
 with open(requirements_path) as reqs:
     for line in reqs.readlines():
-        cmd = "pip3 install " + line.strip()
+        cmd = "pip3 install " + line.strip() + " --use-feature=2020-resolver"
         os.system(cmd)
